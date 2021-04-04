1bbd817dcd257da1c5f00078e51287ff5732f164
Gonca <gonca.negis@gmail.com>
2021-04-04 12:18:26 +0300

    first commit

---
+++ precache-manifest.8b807e53c764284bdef0d985b31f0122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fcb80542932707d0d689a988103506",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7d6206c26188fc76c4",
+    "url": "/static/css/main.03f05d02.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7d6206c26188fc76c4",
+    "url": "/static/js/main.359a4802.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
